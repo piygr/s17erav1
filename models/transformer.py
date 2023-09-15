55ce1716b03cc6b038d18786e7c6975bff647f8b
piygr <grover1piyush@gmail.com>
2023-09-16 02:03:54 +0530

    added code

---
+++ models/transformer.py
@@ -0,0 +1,253 @@
+# =============================================================================
+# Libs
+# =============================================================================
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import math
+
+
+# 1. Create a class which subclasses nn.Module
+class PatchEmbedding(nn.Module):
+    """Turns a 2D input image into a 1D sequence learnable embedding vector.
+
+    Args:
+        in_channels (int): Number of color channels for the input images. Defaults to 3.
+        patch_size (int): Size of patches to convert input image into. Defaults to 16.
+        embedding_dim (int): Size of embedding to turn image into. Defaults to 768.
+    """
+
+    # 2. Initialize the class with appropriate variables
+    def __init__(self,
+                 in_channels: int = 3,
+                 patch_size: int = 16,
+                 embedding_dim: int = 768):
+        super().__init__()
+
+        self.patch_size = patch_size
+        # 3. Create a layer to turn an image into patches
+        self.patcher = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=embedding_dim,
+                                 kernel_size=patch_size,
+                                 stride=patch_size,
+                                 padding=0)
+
+        # 4. Create a layer to flatten the patch feature maps into a single dimension
+        self.flatten = nn.Flatten(start_dim=2,  # only flatten the feature map dimensions into a single vector
+                                  end_dim=3)
+
+    # 5. Define the forward method
+    def forward(self, x):
+        # Create assertion to check that inputs are the correct shape
+        image_resolution = x.shape[-1]
+        assert image_resolution % self.patch_size == 0, f"Input image size must be divisble by patch size, image shape: {image_resolution}, patch size: {patch_size}"
+
+        # Perform the forward pass
+        x_patched = self.patcher(x)
+        x_flattened = self.flatten(x_patched)
+        # 6. Make sure the output shape has the right order
+        return x_flattened.permute(0, 2, 1)
+        # adjust so the embedding is on the final dimension [batch_size, P^2•C, N] -> [batch_size, N, P^2•C]
+
+
+# =============================================================================
+# Transformer
+# =============================================================================
+def attention(q, k, v, mask=None, dropout=None):
+    scores = q.matmul(k.transpose(-2, -1))
+    scores /= math.sqrt(q.shape[-1])
+
+    # mask
+    scores = scores if mask is None else scores.masked_fill(mask == 0, -1e3)
+
+    scores = F.softmax(scores, dim=-1)
+    scores = dropout(scores) if dropout is not None else scores
+    output = scores.matmul(v)
+    return output
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, out_dim, dropout=0.1):
+        super().__init__()
+
+        #        self.q_linear = nn.Linear(out_dim, out_dim)
+        #        self.k_linear = nn.Linear(out_dim, out_dim)
+        #        self.v_linear = nn.Linear(out_dim, out_dim)
+        self.linear = nn.Linear(out_dim, out_dim * 3)
+
+        self.n_heads = n_heads
+        self.out_dim = out_dim
+        self.out_dim_per_head = out_dim // n_heads
+        self.out = nn.Linear(out_dim, out_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def split_heads(self, t):
+        return t.reshape(t.shape[0], -1, self.n_heads, self.out_dim_per_head)
+
+    def forward(self, x, y=None, mask=None):
+        # in decoder, y comes from encoder. In encoder, y=x
+        y = x if y is None else y
+
+        qkv = self.linear(x)  # BS * SEQ_LEN * (3*EMBED_SIZE_L)
+        q = qkv[:, :, :self.out_dim]  # BS * SEQ_LEN * EMBED_SIZE_L
+        k = qkv[:, :, self.out_dim:self.out_dim * 2]  # BS * SEQ_LEN * EMBED_SIZE_L
+        v = qkv[:, :, self.out_dim * 2:]  # BS * SEQ_LEN * EMBED_SIZE_L
+
+        # break into n_heads
+        q, k, v = [self.split_heads(t) for t in (q, k, v)]  # BS * SEQ_LEN * HEAD * EMBED_SIZE_P_HEAD
+        q, k, v = [t.transpose(1, 2) for t in (q, k, v)]  # BS * HEAD * SEQ_LEN * EMBED_SIZE_P_HEAD
+
+        # n_heads => attention => merge the heads => mix information
+        scores = attention(q, k, v, mask, self.dropout)  # BS * HEAD * SEQ_LEN * EMBED_SIZE_P_HEAD
+        scores = scores.transpose(1, 2).contiguous().view(scores.shape[0], -1,
+                                                          self.out_dim)  # BS * SEQ_LEN * EMBED_SIZE_L
+        out = self.out(scores)  # BS * SEQ_LEN * EMBED_SIZE
+
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, inp_dim, inner_dim, dropout=0.1):
+        super().__init__()
+        self.linear1 = nn.Linear(inp_dim, inner_dim)
+        self.linear2 = nn.Linear(inner_dim, inp_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # inp => inner => relu => dropout => inner => inp
+        return self.linear2(self.dropout(F.relu(self.linear1(x))))
+
+
+class TransformerLayer(nn.Module):
+    def __init__(self, n_heads, inner_transformer_size, inner_ff_size, dropout=0.1):
+        super().__init__()
+        self.mha = MultiHeadAttention(n_heads, inner_transformer_size, dropout)
+        self.ff = FeedForward(inner_transformer_size, inner_ff_size, dropout)
+        self.norm1 = nn.LayerNorm(inner_transformer_size)
+        self.norm2 = nn.LayerNorm(inner_transformer_size)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        x2 = self.norm1(x)
+        x = x + self.dropout1(self.mha(x2, mask=mask))
+        x2 = self.norm2(x)
+        x = x + self.dropout2(self.ff(x2))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self,
+                 n_code,
+                 n_heads,
+                 embed_size,
+                 inner_ff_size,
+                 vocab_size,
+                 seq_len,
+                 dropout=.1,
+                 patch_embedding=None,
+                 class_embedding=None):
+        super().__init__()
+
+        if not patch_embedding:
+            self.token_embedding_table = nn.Embedding(vocab_size, embed_size)
+
+        self.patch_embedding = patch_embedding
+        self.class_embedding = class_embedding
+
+        # each position from 0 to block_size-1 will get its embedding
+        self.position_embedding_table = nn.Embedding(seq_len, embed_size)
+
+        # backbone
+        layers = []
+        for i in range(n_code):
+            layers += [TransformerLayer(n_heads, embed_size, inner_ff_size, dropout)]
+        self.layers = nn.ModuleList(layers)
+
+        # language model
+        self.norm = nn.LayerNorm(embed_size)
+        self.linear = nn.Linear(embed_size, vocab_size, bias=False)
+
+    '''
+    def forward(self, x):
+        x = self.embeddings(x)
+        x = x + self.pe(x)
+        for encoder in self.encoders:
+            x = encoder(x)
+        x = self.norm(x)
+        x = self.linear(x)
+        return x
+    '''
+
+
+    def forward(self, idx, targets=None, mask=None):
+
+        if not self.patch_embedding:
+            B, T = idx.shape
+            # x and targets are (B,T) tensor of integers
+            # the token_emb is (B, T, C), C = NUM_EMBED
+
+            token_emb = self.token_embedding_table(idx) #B,T,C
+            # (T, C)
+            posit_emb = self.position_embedding_table(torch.arange(T))
+
+            x = token_emb + posit_emb
+
+        else:
+            B, C, H, W = idx.shape
+            print('Input: ', idx.shape)
+            class_token = self.class_embedding.expand(B, -1, -1)
+            print('Class token: ', class_token.shape)
+            # "-1" means to infer the dimension (try this line on its own)
+
+            x = self.patch_embedding(idx) #B, NUM_PATCH, C=embed_size
+            print('Patch Embedding: ', x.shape)
+
+            x = torch.cat((class_token, x), dim=1)  #B, 1+NUM_PATCH, C
+            print('1+PatchEmbed', x.shape)
+
+            posit_emb = self.position_embedding_table(torch.arange(x.shape[1]))
+            print('Posit Embed: ', posit_emb.shape)
+
+            x = posit_emb + x
+            print('Posit Emb+x: ', x.shape)
+
+        # apply one head of self-attention
+        for layer in self.layers:
+            x = layer(x, mask=mask)
+
+        # (B, T, vocab_size)
+        logits = self.linear(self.norm(x))
+
+        # compute the loss
+        if targets != None:
+            # cross_entropy accepts inputs in a (batch_size, num_classes)
+            # so we need to reformat our logits dimensions to
+            # (batch_size * time, dim_vocabulary), time = block_size
+            B, T, C = logits.shape
+            logits = torch.reshape(logits, (B * T, C) )
+            targets = torch.reshape(targets, (B * T,) )
+            loss = F.cross_entropy(logits, targets)
+        else:
+            loss = None
+
+        return logits, loss
+
+
+    def generate(self, idx: torch.Tensor, max_new_tokens: int, seq_len: int):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop the context too the  last block_size tokens
+            # because tokens don't communicate between blocks
+            idx_crop = idx[:, -seq_len:]
+            # get the predictions
+            logits, loss = self.forward(idx_crop)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution with probabilities probs
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
